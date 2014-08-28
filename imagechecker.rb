6de2275ca731315a55ba53c14fda35a1bff18fca
nelliemckesson <nellie.mckesson@macmillan.com>
2014-08-28 14:47:28 -0400

    adding ruby

---
+++ imagechecker.rb
@@ -0,0 +1,45 @@
+input_file = ARGV[0]
+working_dir_split = ARGV[0].split("\\")
+working_dir = working_dir_split[0...-2].join("\\")
+tmp_id = File.read("#{input_file}").match(/978-?(\d{1}-?){10}/i)
+tmp_dir = "S:\\resources\\bookmaker_tmp"
+
+html_file = "#{tmp_dir}\\#{tmp_id}\\outputtmp.html"
+pisbn = File.read("#{html_file}").scan(/Print ISBN:.*?<\/p>/).to_s.gsub(/-/,"").gsub(/Print ISBN: /,"").gsub(/<\/p>/,"").gsub(/\["/,"").gsub(/"\]/,"")
+
+# The location where the images are dropped by the user
+imagedir = "#{working_dir}\\submitted_images"
+
+# An array listing all the submitted images
+images = Dir["#{imagedir}*"]
+
+# Removes the full path from each image in the array of submitted images, to make matching more precise
+images.each do |s|
+    s.gsub!(/#{imagedir}/,"")
+end
+
+# An array of all the image files referenced in the source html file
+source = File.read("#{html_file}").scan(/img src=".*?"/)
+
+# An empty array to store the list of any missing images
+missing = []
+
+# Checks to see if each image referenced in the html exists in the submission folder
+# If no, saves the image file name in the missing array
+# If yes, copies the image file to the done/pisbn/images folder, and deletes original
+source.each do |m|
+	match = m.split("/").pop.gsub(/"/,'')
+	if images.include?("#{match}")
+		`copy #{working_dir}\\submitted_images\\#{match} #{working_dir}\\done\\#{pisbn}\\images\\`
+		`del #{working_dir}\\submitted_images\\#{match}`
+	else
+		missing << match
+	end
+end
+
+# Writes an error text file in the done\pisbn\ folder that lists all missing image files as stored in the missing array
+if missing.any?
+	File.open("#{working_dir}\\done\\#{pisbn}\\IMAGE_ERROR.txt", 'w') do |output|
+		output.write "The following images are missing from the images folder: #{missing}"
+	end
+end
