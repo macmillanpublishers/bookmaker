98290f85f6154aeeb25b0e6684a05bb30819774a
nelliemckesson <nellie.mckesson@macmillan.com>
2014-09-02 16:24:07 -0400

    fixing htmlfile location

--- filearchive.rb
+++ filearchive.rb
@@ -4,8 +4,14 @@ working_dir = working_dir_split[0...-2].join("\\")
 tmp_id = File.read("#{input_file}").match(/978-?(\d{1}-?){10}/i)
 tmp_dir = "S:\\resources\\bookmaker_tmp"
 
-html_file = "#{tmp_dir}\\#{tmp_id}\\outputtmp.html"
+html_file = "#{tmp_dir}\\outputtmp.html"
 pisbn = File.read("#{html_file}").scan(/Print ISBN:.*?<\/p>/).to_s.gsub(/-/,"").gsub(/Print ISBN: /,"").gsub(/<\/p>/,"").gsub(/\["/,"").gsub(/"\]/,"")
+
+# Rename and move input files to tmp folder to eliminate possibility of overwriting
+`md #{tmp_dir}\\#{pisbn}`
+`move #{working_dir}\\submitted_images\\cover.jpg #{tmp_dir}\\#{pisbn}\\#{pisbn}_cover.jpg`
+`copy #{tmp_dir}\\outputtmp.html #{tmp_dir}\\#{pisbn}\\outputtmp.html`
+`copy #{input_file} #{tmp_dir}\\#{pisbn}\\#{pisbn}.xml`
 
 # create the archival directory structure and copy xml and html there
 `md #{working_dir}\\done\\#{pisbn}`
