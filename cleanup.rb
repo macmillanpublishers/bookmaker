a95aada433f7d47ae9117e63d4b4b08d3222059e
nelliemckesson <nellie.mckesson@macmillan.com>
2014-09-02 16:23:15 -0400

    fixing htmlfile location

--- cleanup.rb
+++ cleanup.rb
@@ -4,13 +4,17 @@ working_dir = working_dir_split[0...-2].join("\\")
 tmp_id = File.read("#{input_file}").match(/978-?(\d{1}-?){10}/i)
 tmp_dir = "S:\\resources\\bookmaker_tmp"
 
+html_file = "#{tmp_dir}\\outputtmp.html"
+pisbn = File.read("#{html_file}").scan(/Print ISBN:.*?<\/p>/).to_s.gsub(/-/,"").gsub(/Print ISBN: /,"").gsub(/<\/p>/,"").gsub(/\["/,"").gsub(/"\]/,"")
+
 # Delete all the working files and dirs
-`del /f /s /q /a #{tmp_dir}\\#{tmp_id}\\OEBPS\\*`
-`rd #{tmp_dir}\\#{tmp_id}\\OEBPS\\`
-`del /f /s /q /a #{tmp_dir}\\#{tmp_id}\\META-INF\\*`
-`rd #{tmp_dir}\\#{tmp_id}\\META-INF\\`
-`del /f /s /q /a #{tmp_dir}\\#{tmp_id}\\mimetype`
-`del /f /s /q /a #{tmp_dir}\\#{tmp_id}\\*`
-`rd #{tmp_dir}\\#{tmp_id}\\`
+`del /f /s /q /a #{tmp_dir}\\#{pisbn}\\OEBPS\\*`
+`rd #{tmp_dir}\\#{pisbn}\\OEBPS\\`
+`del /f /s /q /a #{tmp_dir}\\#{pisbn}\\META-INF\\*`
+`rd #{tmp_dir}\\#{pisbn}\\META-INF\\`
+`del /f /s /q /a #{tmp_dir}\\#{pisbn}\\mimetype`
+`del /f /s /q /a #{tmp_dir}\\#{pisbn}\\*`
+`rd #{tmp_dir}\\#{pisbn}\\`
+`del /f /s /q /a #{html_file}`
 `del /f /s /q /a #{input_file}`
 `del /f /s /q /a #{working_dir}\\IN_USE_PLEASE_WAIT.txt`
