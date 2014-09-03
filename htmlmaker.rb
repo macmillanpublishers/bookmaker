b8331d7a916a02c9abb37771e866b5db0d63576d
nelliemckesson <nellie.mckesson@macmillan.com>
2014-09-03 16:09:19 -0400

    updating for unique tmp dir per input file

--- htmlmaker.rb
+++ htmlmaker.rb
@@ -1,14 +1,9 @@
 input_file = ARGV[0]
+filename_split = input_file.split("/").pop
+filename = filename_split.split(".").shift.gsub(/ /, "")
 working_dir_split = ARGV[0].split("\\")
 working_dir = working_dir_split[0...-2].join("\\")
-tmp_id = File.read("#{input_file}").match(/978-?(\d{1}-?){10}/i)
-tmp_dir = "S:\\resources\\bookmaker_tmp"
-
-# Add a notice to the conversion dir warning that the process is in use
-File.open("#{working_dir}\\IN_USE_PLEASE_WAIT.txt", 'w') do |output|
-	output.write "The conversion processor is currently running. Please do not submit any new files or images until the process completes."
-end
+tmp_dir = "C:\\bookmaker_tmp"
 
 # convert xml to html
-`java -jar C:\\saxon\\saxon9pe.jar -s:#{input_file} -xsl:S:\\resources\\bookmaker_scripts\\wordtohtml.xsl -o:#{tmp_dir}\\outputtmp.html`
-
+`java -jar C:\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\#{filename}.xml -xsl:S:\\resources\\bookmaker_scripts\\wordtohtml.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
