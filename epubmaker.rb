f484ff43629070ad8a4bc83934a57cb755e7b0de
nelliemckesson <nellie.mckesson@macmillan.com>
2014-09-03 10:59:41 -0400

    fixing epub zip temp location

--- epubmaker.rb
+++ epubmaker.rb
@@ -16,6 +16,9 @@ pisbn = File.read("#{html_file}").scan(/Print ISBN:.*?<\/p>/).to_s.gsub(/-/,"").
 # finding imprint name
 imprint = File.read("#{html_file}").scan(/<p class="TitlepageImprintLineimp">.*?<\/p>/).to_s.gsub(/\["<p class=\\"TitlepageImprintLineimp\\">/,"").gsub(/"\]/,"").gsub(/<\/p>/,"")
 
+# epub_dir = "#{tmp_dir}\\#{pisbn}"
+epub_dir = "."
+
 # Adding author meta element to head
 # Replacing toc with empty nav, as required by htmlbook xsl
 filecontents = File.read("#{html_file}").gsub(/<\/head>/,"<meta name='author' content='#{authorname2}' /><meta name='publisher' content='#{imprint}' /><meta name='isbn-13' content='#{eisbn}' /></head>").gsub(/<body data-type="book">/,"<body data-type=\"book\"><figure data-type=\"cover\"><img src=\"cover.jpg\"/></figure>").gsub(/<nav.*<\/nav>/,"<nav data-type='toc' />").gsub(/&nbsp;/,"&#160;")
@@ -26,30 +29,30 @@ File.open("#{tmp_dir}\\#{pisbn}\\epub_tmp.html", 'w') do |output|
 end
 
 # convert to epub
-`java -jar C:\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{pisbn}\\epub_tmp.html -xsl:S:\\resources\\HTMLBook\\HTMLBook-master\\htmlbook-xsl\\epub.xsl -o:#{tmp_dir}\\#{pisbn}\\tmp.epub`
+`java -jar C:\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{pisbn}\\epub_tmp.html -xsl:S:\\resources\\HTMLBook\\HTMLBook-master\\htmlbook-xsl\\epub.xsl -o:#{epub_dir}\\tmp.epub`
 
 # fix cover.html doctype
-covercontents = File.read("#{tmp_dir}\\#{pisbn}\\OEBPS\\cover.html")
+covercontents = File.read("#{epub_dir}\\OEBPS\\cover.html")
 replace = covercontents.gsub(/&lt;!DOCTYPE html&gt;/,"<!DOCTYPE html>")
-File.open("#{tmp_dir}\\#{pisbn}\\OEBPS\\cover.html", "w") {|file| file.puts replace}
+File.open("#{epub_dir}\\OEBPS\\cover.html", "w") {|file| file.puts replace}
 
 # fix author info in opf
-covercontents = File.read("#{tmp_dir}\\#{pisbn}\\OEBPS\\content.opf")
+covercontents = File.read("#{epub_dir}\\OEBPS\\content.opf")
 replace = covercontents.gsub(/<dc:creator/,"<dc:identifier id='isbn'>#{eisbn}</dc:identifier><dc:creator role='aut'")
-File.open("#{tmp_dir}\\#{pisbn}\\OEBPS\\content.opf", "w") {|file| file.puts replace}
+File.open("#{epub_dir}\\OEBPS\\content.opf", "w") {|file| file.puts replace}
 
 # add epub css to epub folder
-`copy #{working_dir}\\done\\#{pisbn}\\layout\\epub.css #{tmp_dir}\\#{pisbn}\\OEBPS\\`
+`copy #{working_dir}\\done\\#{pisbn}\\layout\\epub.css #{epub_dir}\\OEBPS\\`
 
 # add cover image file to epub folder
-`copy #{working_dir}\\done\\#{pisbn}\\cover\\cover.jpg #{tmp_dir}\\#{pisbn}\\OEBPS\\`
+`copy #{working_dir}\\done\\#{pisbn}\\cover\\cover.jpg #{epub_dir}\\OEBPS\\`
 
 #copy tor logo image file to epub folder
-`copy #{working_dir}\\resources\\torDOTcom\\img\\torlogo.jpg #{tmp_dir}\\#{pisbn}\\OEBPS\\`
+`copy #{working_dir}\\resources\\torDOTcom\\img\\torlogo.jpg #{epub_dir}\\OEBPS\\`
 
 # zip epub
-`C:\\zip\\zip.exe #{eisbn}_EPUB.epub -DX0 #{tmp_dir}\\#{pisbn}\\mimetype`
-`C:\\zip\\zip.exe #{eisbn}_EPUB.epub -rDX9 #{tmp_dir}\\#{pisbn}\\META-INF #{tmp_dir}\\#{pisbn}\\OEBPS`
+`C:\\zip\\zip.exe #{eisbn}_EPUB.epub -DX0 #{epub_dir}\\mimetype`
+`C:\\zip\\zip.exe #{eisbn}_EPUB.epub -rDX9 #{epub_dir}\\META-INF #{epub_dir}\\OEBPS`
 
 # move epub into archive folder
 `copy #{eisbn}_EPUB.epub #{working_dir}\\done\\#{pisbn}\\`
