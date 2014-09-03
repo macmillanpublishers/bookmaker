84be529311e22f13d9cb3e510280fc0b1fbbf182
nelliemckesson <nellie.mckesson@macmillan.com>
2014-09-03 16:11:08 -0400

    updating for unique tmp dir per input file

--- cleanup.rb
+++ cleanup.rb
@@ -1,20 +1,21 @@
 input_file = ARGV[0]
+filename_split = input_file.split("/").pop
+filename = filename_split.split(".").shift.gsub(/ /, "")
 working_dir_split = ARGV[0].split("\\")
 working_dir = working_dir_split[0...-2].join("\\")
-tmp_id = File.read("#{input_file}").match(/978-?(\d{1}-?){10}/i)
-tmp_dir = "S:\\resources\\bookmaker_tmp"
+tmp_dir = "C:\\bookmaker_tmp"
 
-html_file = "#{tmp_dir}\\outputtmp.html"
+html_file = "#{tmp_dir}\\#{filename}\\outputtmp.html"
 pisbn = File.read("#{html_file}").scan(/Print ISBN:.*?<\/p>/).to_s.gsub(/-/,"").gsub(/Print ISBN: /,"").gsub(/<\/p>/,"").gsub(/\["/,"").gsub(/"\]/,"")
 
 # Delete all the working files and dirs
-`del /f /s /q /a #{tmp_dir}\\#{pisbn}\\OEBPS\\*`
-`rd #{tmp_dir}\\#{pisbn}\\OEBPS\\`
-`del /f /s /q /a #{tmp_dir}\\#{pisbn}\\META-INF\\*`
-`rd #{tmp_dir}\\#{pisbn}\\META-INF\\`
-`del /f /s /q /a #{tmp_dir}\\#{pisbn}\\mimetype`
-`del /f /s /q /a #{tmp_dir}\\#{pisbn}\\*`
-`rd #{tmp_dir}\\#{pisbn}\\`
+`del /f /s /q /a #{tmp_dir}\\#{filename}\\OEBPS\\*`
+`rd #{tmp_dir}\\#{filename}\\OEBPS\\`
+`del /f /s /q /a #{tmp_dir}\\#{filename}\\META-INF\\*`
+`rd #{tmp_dir}\\#{filename}\\META-INF\\`
+`del /f /s /q /a #{tmp_dir}\\#{filename}\\mimetype`
+`del /f /s /q /a #{tmp_dir}\\#{filename}\\*`
+`rd #{tmp_dir}\\#{filename}\\`
 `del /f /s /q /a #{html_file}`
 `del /f /s /q /a #{input_file}`
 `del /f /s /q /a #{working_dir}\\IN_USE_PLEASE_WAIT.txt`
