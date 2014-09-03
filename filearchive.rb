a3ba9a53b55f2046b92d8493509ef09253a10753
nelliemckesson <nellie.mckesson@macmillan.com>
2014-09-03 16:09:36 -0400

    updating for unique tmp dir per input file

--- filearchive.rb
+++ filearchive.rb
@@ -1,17 +1,12 @@
 input_file = ARGV[0]
+filename_split = input_file.split("/").pop
+filename = filename_split.split(".").shift.gsub(/ /, "")
 working_dir_split = ARGV[0].split("\\")
 working_dir = working_dir_split[0...-2].join("\\")
-tmp_id = File.read("#{input_file}").match(/978-?(\d{1}-?){10}/i)
-tmp_dir = "S:\\resources\\bookmaker_tmp"
+tmp_dir = "C:\\bookmaker_tmp"
 
-html_file = "#{tmp_dir}\\outputtmp.html"
+html_file = "#{tmp_dir}\\#{filename}\\outputtmp.html"
 pisbn = File.read("#{html_file}").scan(/Print ISBN:.*?<\/p>/).to_s.gsub(/-/,"").gsub(/Print ISBN: /,"").gsub(/<\/p>/,"").gsub(/\["/,"").gsub(/"\]/,"")
-
-# Rename and move input files to tmp folder to eliminate possibility of overwriting
-`md #{tmp_dir}\\#{pisbn}`
-`move #{working_dir}\\submitted_images\\cover.jpg #{tmp_dir}\\#{pisbn}\\#{pisbn}_cover.jpg`
-`copy #{tmp_dir}\\outputtmp.html #{tmp_dir}\\#{pisbn}\\outputtmp.html`
-`copy #{input_file} #{tmp_dir}\\#{pisbn}\\#{pisbn}.xml`
 
 # create the archival directory structure and copy xml and html there
 `md #{working_dir}\\done\\#{pisbn}`
