cbdd2c11e8d44efd5a1b003c30b478e32ae156c0
nelliemckesson <nellie.mckesson@macmillan.com>
2014-09-03 16:10:25 -0400

    updating for unique tmp dir per input file

--- chapterheads.rb
+++ chapterheads.rb
@@ -1,10 +1,11 @@
 input_file = ARGV[0]
+filename_split = input_file.split("/").pop
+filename = filename_split.split(".").shift.gsub(/ /, "")
 working_dir_split = ARGV[0].split("\\")
 working_dir = working_dir_split[0...-2].join("\\")
-tmp_id = File.read("#{input_file}").match(/978-?(\d{1}-?){10}/i)
-tmp_dir = "S:\\resources\\bookmaker_tmp"
+tmp_dir = "C:\\bookmaker_tmp"
 
-html_file = "#{tmp_dir}\\outputtmp.html"
+html_file = "#{tmp_dir}\\#{filename}\\outputtmp.html"
 pisbn = File.read("#{html_file}").scan(/Print ISBN:.*?<\/p>/).to_s.gsub(/-/,"").gsub(/Print ISBN: /,"").gsub(/<\/p>/,"").gsub(/\["/,"").gsub(/"\]/,"")
 
 # an array of all occurances of chapters in the manuscript
