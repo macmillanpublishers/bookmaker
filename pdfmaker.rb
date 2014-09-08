5fb6a47f97220e701154b7d07d07256ac13efa86
nelliemckesson <nellie.mckesson@macmillan.com>
2014-09-08 17:10:52 -0400

    batch commit

--- pdfmaker.rb
+++ pdfmaker.rb
@@ -16,12 +16,9 @@ pisbn = File.read("#{html_file}").scan(/Print ISBN:.*?<\/p>/).to_s.gsub(/-/,"").
 # pdf css to be added to the file that will be sent to docraptor
 css_file = File.read("#{working_dir}\\done\\#{pisbn}\\layout\\pdf.css").to_s
 
-# inserts the css into the head of the html
-pdf_html = File.read("#{html_file}").gsub(/<\/head>/,"<style>#{css_file}</style></head>").gsub(/(<img.*?)(>)/,"\\1/\\2").gsub(/src="images\//,"src=\"ftp://art.macmillanusa.com/Content%20Workflows/NYAutomation/").to_s
-
-File.open("#{working_dir}\\done\\#{pisbn}\\pdf.html", 'w') do |p|
-	p.write "#{pdf_html}"
-end
+# inserts the css into the head of the html, fixes images
+# pdf_html = File.read("#{html_file}").gsub(/<\/head>/,"<style>#{css_file}</style></head>").gsub(/(<img.*?)(>)/,"\\1/\\2").gsub(/src="images\//,"src=\"http://art.macmillanusa.com/Content%20Workflows/NYAutomation/").to_s
+pdf_html = File.read("#{html_file}").gsub(/<\/head>/,"<style>#{css_file}</style></head>").gsub(/(<img.*?)(>)/,"\\1/\\2").gsub(/src="images\//,"src=\"http://nelliemckesson.com/test/").to_s
 
 # sends file to docraptor for conversion
 # currently running in test mode; remove test when css is finalized
@@ -31,9 +28,13 @@ File.open("#{pisbn}.pdf", "w+b") do |f|
                            :name             => "#{pisbn}.pdf",
                            :document_type    => "pdf",
                            :strict			 => "none",
-                           :http_user		 => "bookmaker",
-                           :http_password	 => "***REMOVED***",
-                           :test             => true)
+                           :test             => true,
+	                       :prince_options	 => {
+	                           :http_user		 => "bookmaker",
+	                           :http_password	 => "***REMOVED***"
+							   }
+                       		)
+                           
 end
 
 # moves rendered pdf to archival dir
