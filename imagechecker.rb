642732e04fe52ede5c83552b30fed188234fb0fa
nelliemckesson <nellie.mckesson@macmillan.com>
2014-12-05 10:25:00 -0500

    batch commit

--- imagechecker.rb
+++ imagechecker.rb
@@ -3,7 +3,17 @@ filename_split = input_file.split("\\").pop
 filename = filename_split.split(".").shift.gsub(/ /, "")
 working_dir_split = ARGV[0].split("\\")
 working_dir = working_dir_split[0...-2].join("\\")
-tmp_dir = "C:\\bookmaker_tmp"
+# determine current working volume
+`cd > currvol.txt`
+currvol = File.read("currvol.txt")
+puts currvol
+
+# set working dir based on current volume
+if currvol.include?("S:")
+	tmp_dir = "S:\\bookmaker_tmp"
+else
+	tmp_dir = "C:\\bookmaker_tmp"
+end
 
 html_file = "#{tmp_dir}\\#{filename}\\outputtmp.html"
 pisbn = File.read("#{html_file}").scan(/Print ISBN:.*?<\/p>/).to_s.gsub(/-/,"").gsub(/Print ISBN: /,"").gsub(/<\/p>/,"").gsub(/\["/,"").gsub(/"\]/,"")
