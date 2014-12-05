fa3548f7942066c2c0f87ca56aa90e790b9b6f89
nelliemckesson <nellie.mckesson@macmillan.com>
2014-12-05 09:47:11 -0500

    trying to fix epub path problems

--- epubmaker.rb
+++ epubmaker.rb
@@ -65,11 +65,12 @@ end
 puts "success1"
 
 #copy tor logo image file to epub folder
-`copy #{working_dir}\\resources\\torDOTcom\\img\\torlogo.jpg #{tmp_dir}\\#{filename}\\OEBPS\\`
+`copy S:\\resources\\torDOTcom\\img\\torlogo.jpg #{tmp_dir}\\#{filename}\\OEBPS\\`
 
 puts "success2"
 
 # zip epub
+`C:`
 `chdir #{tmp_dir}\\#{filename} & C:\\zip\\zip.exe #{eisbn}_EPUB.epub -DX0 #{epub_dir}\\mimetype`
 `chdir #{tmp_dir}\\#{filename} & C:\\zip\\zip.exe #{eisbn}_EPUB.epub -rDX9 #{epub_dir}\\META-INF #{epub_dir}\\OEBPS`
 
