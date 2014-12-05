19b74783e3832c936752bdfde3db7578f72d4a69
nelliemckesson <nellie.mckesson@macmillan.com>
2014-12-05 10:24:59 -0500

    batch commit

--- htmlmaker.rb
+++ htmlmaker.rb
@@ -3,7 +3,17 @@ filename_split = input_file.split("\\").pop
 filename = filename_split.split(".").shift.gsub(/ /, "")
 working_dir_split = ARGV[0].split("\\")
 working_dir = working_dir_split[0...-2].join("\\")
-tmp_dir = "C:\\bookmaker_tmp"
+# determine current working volume
+`cd > currvol.txt`
+currvol = File.read("currvol.txt")
+puts currvol
+
+# set working dir based on current volume
+if currvol.include?("S:")
+	tmp_dir = "S:\\bookmaker_tmp"
+else
+	tmp_dir = "C:\\bookmaker_tmp"
+end
 
 # convert xml to html
 `java -jar C:\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\#{filename}.xml -xsl:S:\\resources\\bookmaker_scripts\\WordXML-to-HTML\\wordtohtml.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
