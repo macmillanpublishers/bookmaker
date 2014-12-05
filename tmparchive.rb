01069a7e8e603aa4a8611d501a12ad5c28d3d207
nelliemckesson <nellie.mckesson@macmillan.com>
2014-12-05 10:24:58 -0500

    batch commit

--- tmparchive.rb
+++ tmparchive.rb
@@ -3,7 +3,17 @@ filename_split = input_file.split("\\").pop
 filename = filename_split.split(".").shift.gsub(/ /, "")
 working_dir_split = ARGV[0].split("\\")
 working_dir = working_dir_split[0...-2].join("\\")
-tmp_dir = "C:\\bookmaker_tmp"
+# determine current working volume
+`cd > currvol.txt`
+currvol = File.read("currvol.txt")
+puts currvol
+
+# set working dir based on current volume
+if currvol.include?("S:")
+	tmp_dir = "S:\\bookmaker_tmp"
+else
+	tmp_dir = "C:\\bookmaker_tmp"
+end
 
 # Rename and move input files to tmp folder to eliminate possibility of overwriting
 `md #{tmp_dir}\\#{filename}`
