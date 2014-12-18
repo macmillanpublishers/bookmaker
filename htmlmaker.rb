66b009dbe9d7e488ed447584d15a45dc9fd7b8f9
nelliemckesson <nellie.mckesson@macmillan.com>
2014-12-18 14:50:24 -0500

    scalable currvol selection

--- htmlmaker.rb
+++ htmlmaker.rb
@@ -5,14 +5,11 @@ working_dir_split = ARGV[0].split("\\")
 working_dir = working_dir_split[0...-2].join("\\")
 # determine current working volume
 `cd > currvol.txt`
-currvol = File.read("currvol.txt")
+currpath = File.read("currvol.txt")
+currvol = currpath.split("\\").shift
 
 # set working dir based on current volume
-if currvol.include?("S:")
-	tmp_dir = "S:\\bookmaker_tmp"
-else
-	tmp_dir = "C:\\bookmaker_tmp"
-end
+tmp_dir = "#{currvol}\\bookmaker_tmp"
 
 # convert xml to html
 `java -jar C:\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\#{filename}.xml -xsl:S:\\resources\\bookmaker_scripts\\WordXML-to-HTML\\wordtohtml.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
