e3aa4efbbb42d0a02bb997879e707b222921b0a1
nelliemckesson <nellie.mckesson@macmillan.com>
2015-01-15 12:56:32 -0500

    adding if statement to see if css files exist

--- chapterheads.rb
+++ chapterheads.rb
@@ -18,25 +18,26 @@ pisbn = File.read("#{html_file}").scan(/ISBN\s*.+\s*\(hardcover\)\s*<\/p>/).to_s
 # an array of all occurances of chapters in the manuscript
 chapterheads = File.read("#{html_file}").scan(/section data-type="chapter"/)
 
-# base css files
+# css files
 pdf_css_file = "S:\\resources\\bookmaker_scripts\\bookmaker_pdfmaker\\css\\#{project_dir}\\pdf.css"
 epub_css_file = "S:\\resources\\bookmaker_scripts\\bookmaker_epubmaker\\css\\#{project_dir}\\epub.css"
 
-# read the css files
-pdf_css = File.read("#{pdf_css_file}")
-epub_css = File.read("#{epub_css_file}")
-
-# if number of chapters is greater than 1, copies the css as-is to the archival dir
-# if number of chapters is one or less, adds a style to suppress chapter titles to the css and then copies to the archival dir
-if chapterheads.count > 1
-	`copy #{pdf_css_file} #{working_dir}\\done\\#{pisbn}\\layout\\pdf.css`
-	`copy #{epub_css_file} #{working_dir}\\done\\#{pisbn}\\layout\\epub.css`
-else
-	File.open("#{working_dir}\\done\\#{pisbn}\\layout\\pdf.css", 'w') do |p|
-		p.write "#{pdf_css}section[data-type='chapter']>h1{display:none;}"
-	end
-	File.open("#{working_dir}\\done\\#{pisbn}\\layout\\epub.css", 'w') do |e|
-		e.write "#{epub_css}h1.ChapTitlect{display:none;}"
+if File.file?('#{pdf_css_file}')
+	pdf_css = File.read("#{pdf_css_file}")
+	if chapterheads.count > 1
+		`copy #{pdf_css_file} #{working_dir}\\done\\#{pisbn}\\layout\\pdf.css`
+	else
+		File.open("#{working_dir}\\done\\#{pisbn}\\layout\\pdf.css", 'w') do |p|
+			p.write "#{pdf_css}section[data-type='chapter']>h1{display:none;}"
 	end
 end
 
+if File.file?('#{epub_css_file}')
+	epub_css = File.read("#{epub_css_file}")
+	if chapterheads.count > 1
+		`copy #{epub_css_file} #{working_dir}\\done\\#{pisbn}\\layout\\epub.css`
+	else
+		File.open("#{working_dir}\\done\\#{pisbn}\\layout\\epub.css", 'w') do |e|
+			e.write "#{epub_css}h1.ChapTitlect{display:none;}"
+	end
+end
