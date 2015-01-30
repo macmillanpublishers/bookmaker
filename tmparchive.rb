c9943bd82e458794f27931fe043cd0abe2046465
nelliemckesson <nellie.mckesson@macmillan.com>
2015-01-30 15:25:36 -0500

    adding tests

--- tmparchive.rb
+++ tmparchive.rb
@@ -11,12 +11,70 @@ currvol = currpath.split("\\").shift
 # set working dir based on current volume
 tmp_dir = "#{currvol}\\bookmaker_tmp"
 
+# For TEST purposes
+test_images_before = Dir.entries("#{working_dir}\\submitted_images\\")
+
 # Rename and move input files to tmp folder to eliminate possibility of overwriting
 `md #{tmp_dir}\\#{filename}`
-`move #{working_dir}\\submitted_images\\cover.jpg #{tmp_dir}\\#{filename}\\cover.jpg`
+`md #{tmp_dir}\\#{filename}\\images`
+`move #{working_dir}\\submitted_images\\* #{tmp_dir}\\#{filename}\\images\\`
 `copy #{input_file} #{tmp_dir}\\#{filename}\\#{filename}.xml`
 
 # Add a notice to the conversion dir warning that the process is in use
 File.open("#{working_dir}\\IN_USE_PLEASE_WAIT.txt", 'w') do |output|
 	output.write "The conversion processor is currently running. Please do not submit any new files or images until the process completes."
+end
+
+# TESTING
+
+# Filename should not be null
+test_fname = filename.split(%r{\s*})
+
+if test_fname.length != 0
+	test_fname_status = "pass: original filename is not null"
+else
+	test_fname_status = "FAIL: original filename is not null"
+end
+
+# tmpdir should exist
+if File.exist?("#{tmp_dir}\\#{filename}") and File.exist?("#{tmp_dir}\\#{filename}\\images")
+	test_dir_status = "pass: temp directory and all sub-directories were successfully created"
+else
+	test_dir_status = "FAIL: temp directory and all sub-directories were successfully created"
+end
+
+# submitted images dir should be clean
+test_images_after = Dir.entries("#{working_dir}\\submitted_images\\")
+
+if test_images_after.length == 2
+	test_imagedir_status = "pass: submitted images directory has been emptied"
+else
+	test_imagedir_status = "FAIL: submitted images directory has been emptied"
+end
+
+# IF submitted images dir was not clean at beginning, tmpdir images dir should also not be clean at end
+test_tmp_images = Dir.entries("#{tmp_dir}\\#{filename}\\images\\")
+
+if test_images_before.length == test_tmp_images.length
+	test_tmpimgdir_status = "pass: all submitted images have been copied to temp directory"
+else
+	test_tmpimgdir_status = "FAIL: all submitted images have been copied to temp directory"
+end
+
+# input file should exist in tmp dir
+if File.file?("#{tmp_dir}\\#{filename}\\#{filename}.xml")
+	test_input_status = "pass: original file preserved in project directory"
+else
+	test_input_status = "FAIL: original file preserved in project directory"
+end
+
+# Write test results
+File.open("S:\\resources\\logs\\#{filename}.txt", 'a+') do |f|
+	f.puts Time.now
+	f.puts "TMPARCHIVE PROCESSES"
+	f.puts test_fname_status
+	f.puts test_dir_status
+	f.puts test_imagedir_status
+	f.puts test_tmpimgdir_status
+	f.puts test_input_status
 end
