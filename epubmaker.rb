11f926eb74ff1f169b8471da5e6a3e0c1826da72
nelliemckesson <nellie.mckesson@macmillan.com>
2015-02-10 10:17:49 -0500

    trying to fix eisbn

--- epubmaker.rb
+++ epubmaker.rb
@@ -20,7 +20,7 @@ authorname1 = File.read("#{html_file}").scan(/<p class="TitlepageAuthorNameau">.
 authorname2 = authorname1.gsub(/<p class="TitlepageAuthorNameau">/,"").gsub(/</,"")
 
 # finding both print and ebook isbns
-eisbn_basestring = File.read("#{html_file}").scan(/ISBN\s*.+\s*\(e(-?)book\)/).to_s.gsub(/-/,"").gsub(/\s+/,"").gsub(/\["/,"").gsub(/"\]/,"")
+eisbn_basestring = File.read("#{html_file}").scan(/ISBN\s*.+\s*\(e-*book\)/).to_s.gsub(/-/,"").gsub(/\s+/,"").gsub(/\["/,"").gsub(/"\]/,"")
 eisbn = eisbn_basestring.scan(/\d+\(ebook\)/).to_s.gsub(/\(ebook\)/,"").gsub(/\["/,"").gsub(/"\]/,"")
 
 # determing print isbn
@@ -121,6 +121,6 @@ File.open("S:\\resources\\logs\\#{filename}.txt", 'a+') do |f|
 	f.puts " "
 	f.puts "-----"
 	f.puts test_epub_status
-	f.puts test_eisbn_status
 	f.puts "----- ebook ISBN: #{eisbn}"
+	f.puts test_eisbn_status
 end
