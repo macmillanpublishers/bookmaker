bd5d45e03b1bf08475261ebe5e013d8c692964a6
mattretzer <matthew.retzer@macmillan.com>
2015-03-13 12:14:23 -0400

    added imgeupload call, updated http path

--- pdfmaker.rb
+++ pdfmaker.rb
@@ -40,11 +40,14 @@ if pisbn.length == 0
   pisbn = "#{filename}"
 end
 
+#upload images to macmillan.tools site
+`S:\\resources\\bookmaker_scripts\\bookmaker_ftpupload\\imageupload.bat #{working_dir}\\done\\#{pisbn}\\images\\`
+
 # pdf css to be added to the file that will be sent to docraptor
 css_file = File.read("#{working_dir}\\done\\#{pisbn}\\layout\\pdf.css").to_s
 
 # inserts the css into the head of the html, fixes images
-pdf_html = File.read("#{html_file}").gsub(/<\/head>/,"<style>#{css_file}</style></head>").gsub(/(<img.*?)(>)/,"\\1/\\2").gsub(/src="images\//,"src=\"http://www.macmillan.tools.vhost.zerolag.com/bookmaker/tmp_img/").to_s
+pdf_html = File.read("#{html_file}").gsub(/<\/head>/,"<style>#{css_file}</style></head>").gsub(/(<img.*?)(>)/,"\\1/\\2").gsub(/src="images\//,"src=\"http://www.macmillan.tools.vhost.zerolag.com/bookmaker/bookmakerimg/").to_s
 
 # sends file to docraptor for conversion
 # currently running in test mode; remove test when css is finalized
