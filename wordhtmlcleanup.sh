7ef354843174cbc3a7b83b2374add9c2a87b2819
nelliemckesson <nellie.mckesson@macmillan.com>
2015-04-06 10:29:47 -0400

    removing extraneous file

--- wordhtmlcleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-perl -p -i -e 's/<p class="SectionBreaksbr">.*?<\/p>/<\/section><section>/g' $1
-perl -p -i -e 's/<\/section>?//' $1
-perl -p -i -e 's/<\/section>/<\/section>\n/g' $1
-perl -p -i -e 's/<\/p>/<\/p>\n/g' $1
-#perl -p -i -e 's/<p class="PartStartpts">.*<\/p>/<div data-type="part">/g' $1
-#perl -p -i -e 's/<p class="PartEndpte">.*<\/p>/<\/div>/g' $1
