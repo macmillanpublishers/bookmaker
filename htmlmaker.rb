1620a0551a1a9b3c8e2f78fed313828487afa93b
nelliemckesson <nellie.mckesson@macmillan.com>
2015-04-13 16:34:57 -0400

    reworking all paths to use standard variables

--- htmlmaker.rb
+++ htmlmaker.rb
@@ -1,18 +1,42 @@
+# --------------------STANDARD HEADER START--------------------
+# The bookmkaer scripts require a certain folder structure 
+# in order to source in the correct CSS files, logos, 
+# and other imprint-specific items. You can read about the 
+# required folder structure here:
 input_file = ARGV[0]
 filename_split = input_file.split("\\").pop
 filename = filename_split.split(".").shift.gsub(/ /, "")
 working_dir_split = ARGV[0].split("\\")
 working_dir = working_dir_split[0...-2].join("\\")
-# determine current working volume
+project_dir = working_dir_split[0...-3].pop
+stage_dir = working_dir_split[0...-2].pop
+# In Macmillan's environment, these scripts could be 
+# running either on the C: volume or on the S: volume 
+# of the configured server. This block determines which 
+# of those is the current working volume.
 `cd > currvol.txt`
 currpath = File.read("currvol.txt")
 currvol = currpath.split("\\").shift
 
-# set working dir based on current volume
+# --------------------USER CONFIGURED PATHS START--------------------
+# These are static paths to folders on your system.
+# These paths will need to be updated to reflect your current 
+# directory structure.
+
+# set temp working dir based on current volume
 tmp_dir = "#{currvol}\\bookmaker_tmp"
+# set directory for logging output
+log_dir = "S:\\resources\\logs"
+# set directory where bookmkaer scripts live
+bookmaker_dir = "S:\\resources\\bookmaker_scripts"
+# set directory where other resources are installed
+# (for example, saxon, zip)
+resource_dir = "C:"
+# --------------------USER CONFIGURED PATHS END--------------------
+# --------------------STANDARD HEADER END--------------------
 
 # convert xml to html
-`java -jar C:\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\#{filename}.xml -xsl:S:\\resources\\bookmaker_scripts\\WordXML-to-HTML\\wordtohtml.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
+`java -jar #{resource_dir}\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\#{filename}.xml -xsl:#{bookmaker_dir}\\WordXML-to-HTML\\wordtohtml.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
 
 # place footnote text inline per htmlbook
 filecontents = File.read("#{tmp_dir}\\#{filename}\\outputtmp.html")
@@ -40,16 +64,16 @@ replace = nbspcontents.gsub(/&nbsp/,"&#160").gsub(/(<img.*?)(>)/,"\\1/\\2")
 File.open("#{tmp_dir}\\#{filename}\\outputtmp.html", "w") {|file| file.puts replace}
 
 # strip extraneous footnote section from html
-`java -jar C:\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\outputtmp.html -xsl:S:\\resources\\bookmaker_scripts\\bookmaker_htmlmaker\\footnotes.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
+`java -jar #{resource_dir}\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\outputtmp.html -xsl:#{bookmaker_dir}\\bookmaker_htmlmaker\\footnotes.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
 
 # strip static toc from html
-`java -jar C:\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\outputtmp.html -xsl:S:\\resources\\bookmaker_scripts\\bookmaker_htmlmaker\\strip-toc.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
+`java -jar #{resource_dir}\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\outputtmp.html -xsl:#{bookmaker_dir}\\bookmaker_htmlmaker\\strip-toc.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
 
 # convert parts to divs
-`java -jar C:\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\outputtmp.html -xsl:S:\\resources\\bookmaker_scripts\\bookmaker_htmlmaker\\parts.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
+`java -jar #{resource_dir}\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\outputtmp.html -xsl:#{bookmaker_dir}\\bookmaker_htmlmaker\\parts.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
 
 # add correct markup for inlines (em, strong, sup, sub)
-`java -jar C:\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\outputtmp.html -xsl:S:\\resources\\bookmaker_scripts\\bookmaker_htmlmaker\\inlines.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
+`java -jar #{resource_dir}\\saxon\\saxon9pe.jar -s:#{tmp_dir}\\#{filename}\\outputtmp.html -xsl:#{bookmaker_dir}\\bookmaker_htmlmaker\\inlines.xsl -o:#{tmp_dir}\\#{filename}\\outputtmp.html`
 
 # TESTING
 
@@ -72,7 +96,7 @@ else
 end
 
 # Printing the test results to the log file
-File.open("S:\\resources\\logs\\#{filename}.txt", 'a+') do |f|
+File.open("#{log_dir}\\#{filename}.txt", 'a+') do |f|
 	f.puts "----- HTMLMAKER PROCESSES"
 	f.puts test_html_status
 	f.puts test_content_status
