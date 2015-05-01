b34c69f8f496c73726ee3d4af4a843464d5a8d16
nelliemckesson <nellie.mckesson@macmillan.com>
2015-05-01 09:36:21 -0400

    updating header

--- tmparchive.rb
+++ tmparchive.rb
@@ -1,39 +1,4 @@
-# --------------------STANDARD HEADER START--------------------
-# The bookmkaer scripts require a certain folder structure 
-# in order to source in the correct CSS files, logos, 
-# and other imprint-specific items. You can read about the 
-# required folder structure here:
-input_file = ARGV[0]
-filename_split = input_file.split("\\").pop
-filename = filename_split.split(".").shift.gsub(/ /, "")
-working_dir_split = ARGV[0].split("\\")
-working_dir = working_dir_split[0...-2].join("\\")
-project_dir = working_dir_split[0...-2].pop.split("_").shift
-stage_dir = working_dir_split[0...-2].pop.split("_").pop
-# In Macmillan's environment, these scripts could be 
-# running either on the C: volume or on the S: volume 
-# of the configured server. This block determines which 
-# of those is the current working volume.
-`cd > currvol.txt`
-currpath = File.read("currvol.txt")
-currvol = currpath.split("\\").shift
-
-# --------------------USER CONFIGURED PATHS START--------------------
-# These are static paths to folders on your system.
-# These paths will need to be updated to reflect your current 
-# directory structure.
-
-# set temp working dir based on current volume
-tmp_dir = "#{currvol}\\bookmaker_tmp"
-# set directory for logging output
-log_dir = "S:\\resources\\logs"
-# set directory where bookmkaer scripts live
-bookmaker_dir = "S:\\resources\\bookmaker_scripts"
-# set directory where other resources are installed
-# (for example, saxon, zip)
-resource_dir = "C:"
-# --------------------USER CONFIGURED PATHS END--------------------
-# --------------------STANDARD HEADER END--------------------
+require '../bookmaker/header.rb'
 
 filename_split_nospaces = filename_split.gsub(/ /, "")
 
