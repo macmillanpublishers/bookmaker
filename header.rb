ba2fb05512972241126b0ddee1ff2e6ff6c7f64b
nelliemckesson <nellie.mckesson@macmillan.com>
2015-05-04 15:39:28 -0400

    beginning to make paths system-agmostic

--- header.rb
+++ header.rb
@@ -4,54 +4,57 @@ module Bkmkr
 		def self.input_file
 			@@input_file
 		end
-		@@filename_split = input_file.split("\\").pop
+		@@filename_split = input_file.split(File::SEPARATOR).pop
 		def self.filename_split
 			@@filename_split
 		end
-		@@filename = input_file.split("\\").pop.split(".").shift.gsub(/ /, "")
+		@@filename = input_file.split(File::SEPARATOR).pop.split(".").shift.gsub(/ /, "")
 		def self.filename
 			@@filename
 		end
-		@@working_dir_split = input_file.split("\\")
+		@@filename_normalized = filename_split.gsub(/ /, "")
+		def self.filename_normalized
+			@@filename_normalized
+		end
+		@@working_dir_split = input_file.split(File::SEPARATOR)
 		def self.working_dir_split
 			@@working_dir_split
 		end
-		@@working_dir = input_file.split("\\")[0...-2].join("\\")
+		@@working_dir = input_file.split(File::SEPARATOR)[0...-2].join(File::SEPARATOR)
 		def self.working_dir
 			@@working_dir
 		end
-		@@project_dir = input_file.split("\\")[0...-2].pop.split("_").shift
+		@@project_dir = input_file.split(File::SEPARATOR)[0...-2].pop.split("_").shift
 		def self.project_dir
 			@@project_dir
 		end
-		@@stage_dir = input_file.split("\\")[0...-2].pop.split("_").pop
+		@@stage_dir = input_file.split(File::SEPARATOR)[0...-2].pop.split("_").pop
 		def self.stage_dir
 			@@stage_dir
 		end
 	end
 
 	class Dir
-
 		def self.currpath
 			`cd`
 		end
 
-		@@currvol = Dir.currpath.split('\\').shift
+		@@currvol = Dir.currpath.split(File::SEPARATOR).shift
 		def self.currvol
 			@@currvol
 		end
 
-		@@tmp_dir = "#{@@currvol}\\bookmaker_tmp"
+		@@tmp_dir = File.join(currvol, "bookmaker_tmp")
 		def self.tmp_dir
 			@@tmp_dir
 		end
 
-		@@log_dir = "S:\\resources\\logs"
+		@@log_dir = File.join("S:", "resources", "logs")
 		def self.log_dir
 			@@log_dir
 		end
 
-		@@bookmaker_dir = "S:\\resources\\bookmaker_scripts"
+		@@bookmaker_dir = File.join("S:", "resources", "bookmaker_scripts")
 		def self.bookmaker_dir
 			@@bookmaker_dir
 		end
@@ -59,6 +62,31 @@ module Bkmkr
 		@@resource_dir = "C:"
 		def self.resource_dir
 			@@resource_dir
+		end
+
+		@@submitted_images = File.join(Project.working_dir, "submitted_images")
+		def self.submitted_images
+			@@submitted_images
+		end
+
+		@@project_tmp_dir = File.join(tmp_dir, Project.filename)
+		def self.project_tmp_dir
+			@@project_tmp_dir
+		end
+
+		@@project_tmp_dir_img = File.join(tmp_dir, Project.filename, "images")
+		def self.project_tmp_dir_img
+			@@project_tmp_dir_img
+		end
+
+		@@project_tmp_file = File.join(tmp_dir, Project.filename, Project.filename_normalized)
+		def self.project_tmp_file
+			@@project_tmp_file
+		end
+
+		@@alert = File.join(Project.working_dir, "IN_USE_PLEASE_WAIT.txt")
+		def self.alert
+			@@alert
 		end
 	end
 
