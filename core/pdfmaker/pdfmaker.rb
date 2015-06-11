b9b1ca92850689d0555dcda7d34f9c5a9dfa1d56
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-11 13:22:30 -0400

    trying to fix tor css

--- core/pdfmaker/pdfmaker.rb
+++ core/pdfmaker/pdfmaker.rb
@@ -27,7 +27,7 @@ end
 # Link to print css in the html head
 cssfile = File.join(Bkmkr::Project.working_dir, "done", Metadata.pisbn, "layout", "pdf.css")
 if File.file?(cssfile)
-	embedcss = File.read(cssfile).gsub(/\\/,"\\\\").to_s
+	embedcss = File.read(cssfile)
 else
 	embedcss = " "
 end
