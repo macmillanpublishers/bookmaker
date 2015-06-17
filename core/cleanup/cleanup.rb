0477983335138621779c1c5d803e7a340122a413
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-17 16:28:07 -0400

    fixing image handling

--- core/cleanup/cleanup.rb
+++ core/cleanup/cleanup.rb
@@ -23,7 +23,6 @@ end
 
 # Delete all the working files and dirs
 FileUtils.rm_r(Bkmkr::Paths.project_tmp_dir)
-FileUtils.rm_rf("#{Bkmkr::Paths.submitted_images}/.")
 FileUtils.rm(Bkmkr::Project.input_file)
 FileUtils.rm(Bkmkr::Paths.alert)
 
--- core/tmparchive/tmparchive.rb
+++ core/tmparchive/tmparchive.rb
@@ -6,7 +6,7 @@ require_relative '../header.rb'
 test_images_before = Dir.entries(Bkmkr::Paths.submitted_images)
 
 # Local path variables
-all_submitted_images = File.join(Bkmkr::Paths.submitted_images, "*")
+all_submitted_images = Dir.entries(Bkmkr::Paths.submitted_images)
 
 # Rename and move input files to tmp folder to eliminate possibility of overwriting
 if Dir.exist?(Bkmkr::Paths.project_tmp_dir)
@@ -14,8 +14,13 @@ if Dir.exist?(Bkmkr::Paths.project_tmp_dir)
 end
 Dir.mkdir(Bkmkr::Paths.project_tmp_dir)
 Dir.mkdir(Bkmkr::Paths.project_tmp_dir_img)
-FileUtils.cp(Dir.glob(all_submitted_images), Bkmkr::Paths.project_tmp_dir_img)
 FileUtils.cp(Bkmkr::Project.input_file, Bkmkr::Paths.project_tmp_file)
+
+all_submitted_images.each do |c|
+	unless c.include?(".db") or c.include?("DS_Store")
+		FileUtils.mv(c, Bkmkr::Paths.project_tmp_dir_img)
+	end
+end
 
 # Add a notice to the conversion dir warning that the process is in use
 File.open("#{Bkmkr::Paths.alert}", 'w') do |output|
