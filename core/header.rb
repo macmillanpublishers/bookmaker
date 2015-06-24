a0d80bae9716ab49e867d2d29431cea7b9d2369b
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-24 11:07:47 -0400

    removing old zip stuff

--- core/header.rb
+++ core/header.rb
@@ -189,24 +189,5 @@ module Bkmkr
 				end
 			end
 		end
-		def self.zip_epub(target_path, zipfilename)
-            if $zip_install_path
-            	zippath = "#{$zip_install_path}"
-            elsif os == "windows"
-                zippath = File.join(Paths.resource_dir, "zip", "zip.exe")              
-            elsif os == "mac" or os == "unix"
-  				zippath == "zip"
-            else
-                File.open(Bkmkr::Paths.log_file, 'a+') do |f|
-                    f.puts "----- ZIP ERROR"
-                    f.puts "ERROR: I can't seem to run zip. Is it installed and part of your system PATH? (or custom path specified in config.rb?)"
-                    f.puts "ABORTING. All following processes will fail."
-                end
-                File.delete(Project.alert)
-            end
-			`cd "#{target_path}"`
-			`#{zippath} #{zipfilename} -DX0 mimetype`
-			`#{zippath} #{zipfilename} -rDX9 META-INF OEBPS`    
-        end   
 	end
 end
