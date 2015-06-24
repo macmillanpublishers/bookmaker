d50b211b4f471dd56802c8f461d38a2ee7bc66a8
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-24 13:23:46 -0400

    seeing if I still need some epub stuff

--- core/epubmaker/epubmaker.rb
+++ core/epubmaker/epubmaker.rb
@@ -66,14 +66,14 @@ replace = opfcontents.gsub(/<dc:creator/,"<dc:identifier id='isbn'>#{Metadata.ei
 File.open("#{OEBPS_dir}/content.opf", "w") {|file| file.puts replace}
 
 # fix toc entry in ncx
-ncxcontents = File.read("#{OEBPS_dir}/toc.ncx")
-replace = ncxcontents.gsub(/<navLabel><text\/><\/navLabel><content src="toc/,"<navLabel><text>Contents</text><\/navLabel><content src=\"toc")
-File.open("#{OEBPS_dir}/toc.ncx", "w") {|file| file.puts replace}
+#ncxcontents = File.read("#{OEBPS_dir}/toc.ncx")
+#replace = ncxcontents.gsub(/<navLabel><text\/><\/navLabel><content src="toc/,"<navLabel><text>Contents</text><\/navLabel><content src=\"toc")
+#File.open("#{OEBPS_dir}/toc.ncx", "w") {|file| file.puts replace}
 
 # fix toc entry in ncx
-htmlcontents = File.read("#{OEBPS_dir}/toc01.html")
-replace = htmlcontents.gsub(/(<li data-type="copyright-page">)/,"<li data-type=\"toc\" class=\"Nonprinting\"><a href=\"toc01.html\">Contents</a></li>\\1").gsub(/(titlepage01.html#.*?">)(.*?)(<\/a>)/,"\\1Title Page\\3")
-File.open("#{OEBPS_dir}/toc01.html", "w") {|file| file.puts replace}
+#htmlcontents = File.read("#{OEBPS_dir}/toc01.html")
+#replace = htmlcontents.gsub(/(<li data-type="copyright-page">)/,"<li data-type=\"toc\" class=\"Nonprinting\"><a href=\"toc01.html\">Contents</a></li>\\1").gsub(/(titlepage01.html#.*?">)(.*?)(<\/a>)/,"\\1Title Page\\3")
+#File.open("#{OEBPS_dir}/toc01.html", "w") {|file| file.puts replace}
 
 # add epub css to epub folder
 FileUtils.cp("#{Bkmkr::Paths.done_dir}/#{Metadata.pisbn}/layout/epub.css", OEBPS_dir)
