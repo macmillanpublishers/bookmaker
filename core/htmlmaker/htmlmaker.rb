2bd524ec690610ade4c463cb39323e832c6a7ba4
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-25 19:59:29 -0400

    adding main xsl to htmlmaker

--- core/htmlmaker/htmlmaker.rb
+++ core/htmlmaker/htmlmaker.rb
@@ -6,7 +6,7 @@ require_relative '../header.rb'
 saxonpath = File.join(Bkmkr::Paths.resource_dir, "saxon", "#{Bkmkr::Tools.xslprocessor}.jar")
 docxtoxml_py = File.join(Bkmkr::Paths.core_dir, "htmlmaker", "docxtoxml.py")
 source_xml = File.join(Bkmkr::Paths.project_tmp_dir, "#{Bkmkr::Project.filename}.xml")
-word_to_html_xsl = File.join(Bkmkr::Paths.scripts_dir, "WordXML-to-HTML", "wordtohtml.xsl")
+word_to_html_xsl = File.join(Bkmkr::Paths.core_dir, "htmlmaker", "wordtohtml.xsl")
 footnotes_xsl = File.join(Bkmkr::Paths.core_dir, "htmlmaker", "footnotes.xsl")
 strip_toc_xsl = File.join(Bkmkr::Paths.core_dir, "htmlmaker", "strip-toc.xsl")
 parts_xsl = File.join(Bkmkr::Paths.core_dir, "htmlmaker", "parts.xsl")
