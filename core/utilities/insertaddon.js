88e734312d4606f8163fe01e6a54ce17b31f3233
nelliemckesson <nellie.mckesson@macmillan.com>
2015-07-29 12:24:50 -0400

    adding addon js to core

---
+++ core/utilities/insertaddon.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var file = process.argv[2];
+var addonContent = process.argv[3];
+var locationContainer = process.argv[4];
+var locationType = process.argv[5];
+var locationClass = process.argv[6];
+var s = process.argv[7] - 1;
+var locationName = process.argv[8];
+
+fs.readFile(file, function insertAddon (err, contents) {
+  $ = cheerio.load(contents);
+  if (locationType && locationClass) { 
+  	var marker = $(locationContainer + '[class="' + locationClass + '"][data-type="' + locationType + '"]')[s]; 
+  } else if (locationType && !locationClass) {
+  	var marker = $(locationContainer + '[data-type="' + locationType + '"]')[s];
+  } else if (!locationType && locationClass) {
+  	var marker = $(locationContainer + '[class="' + locationClass + '"]')[s];
+  } else {
+  	var marker = $(locationContainer)[s];
+  };
+  
+  if (locationName == "endofbook") {
+  	$('body').append(addonContent);
+  } else {
+    $(marker).before(addonContent);
+  };
+
+  var output = $.html();
+	  fs.writeFile(file, output, function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+
+	    console.log("Addon content has been inserted!");
+	});
+});
