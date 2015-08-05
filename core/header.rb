babd60d9efcc426a37804fa722b753100a15f161
nelliemckesson <nellie.mckesson@macmillan.com>
2015-08-05 17:07:06 -0400

    troubleshooting

--- core/header.rb
+++ core/header.rb
@@ -347,13 +347,15 @@ module Bkmkr
 				end
 			end
 
-			# puts "2= #{inputfile}"
-			# puts "3= #{addoncontent}"
-			# puts "4= #{locationcontainer}"
-			# puts "5= #{locationtype}"
-			# puts "6= #{locationclass}"
-			# puts "7= #{sequence}"
-			# puts "8= #{location}"
+			puts "2= #{inputfile}"
+			puts "3= #{srccontainer}"
+			puts "4= #{srctype}"
+			puts "5= #{srcclass}"
+			puts "6= #{srcseq}"
+			puts "7= #{destcontainer}"
+			puts "8= #{desttype}"
+			puts "9= #{destclass}"
+			puts "10= #{destseq}"
 
 			jsfile = File.join(Paths.core_dir, "utilities", "movesection.js")
 
