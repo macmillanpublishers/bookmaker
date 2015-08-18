1a3ee1021f5761c8b0141e896870b0bdcae2647d
nelliemckesson <nellie.mckesson@macmillan.com>
2015-08-18 14:47:56 -0400

    adding link eval code

--- core/utilities/evaltemplates.js
+++ core/utilities/evaltemplates.js
@@ -16,7 +16,40 @@ fs.readFile(file, function processTemplates (err, contents) {
 
     // evaluate the templates
     if (template == "eval-link") {
-      //...Matt's code...
+      var LinkId;
+
+      if (baseArgs.length > 0) {
+        var seq = baseArgs[1] - 1;
+      } else {
+        var seq = 0;
+      };
+
+      if (baseArgs[0] == "abouttheauthor") {
+        if ($('section[class="abouttheauthor"]').length) {
+          Linkid = $('section[class="abouttheauthor"]')[seq].attr('id');
+        } else {
+          $('a[class="eval-link"][id="abouttheauthor"]').parent().remove();
+        };
+      } else if (baseArgs[0] == "copyright-page") {
+        if ($('section[data-type="copyright-page"]').length) {
+          Linkid = $('section[data-type="copyright-page"]')[seq].attr('id');
+        } else {
+          $('a[class="eval-link"][id="copyright-page"]').parent().remove();
+        };
+      } else if (baseArgs[0] == "beginreading") {
+        if ($('section[data-type="introduction"]').length) {
+          Linkid = $('section[data-type="introduction"]')[seq].attr('id');
+        } else if ($('div[data-type="part"]').length) {
+          Linkid = $('div[data-type="part"]')[seq].attr('id');
+        } else {
+          Linkid = $('section[data-type="chapter"]')[seq].attr('id');
+        }
+      } else if (baseArgs[0] == "toc") {
+        LinkId = "z_TOC";
+        $('nav[data-type="toc"]').attr('id', LinkId); 
+      };
+      
+      $('a[class="eval-link"][id="' + baseArgs[0] + '"]').attr('href', LinkId);
 
       // replace sourceEl class so it doesn't get reprocessed
       $(thisNode).attr('class', 'spanhyperlink');
