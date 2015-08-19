23a7ca12d9894eb2c8f068579428abe7c68adf95
nelliemckesson <nellie.mckesson@macmillan.com>
2015-08-19 13:22:59 -0400

    fleshing out node command

--- core/header.rb
+++ core/header.rb
@@ -206,7 +206,19 @@ module Bkmkr
 			end
 		end
 		def self.runnode(js, html)
-			`node #{js} #{html}`
+			if os == "mac" or os == "unix"
+				`node #{js} #{html}`
+			elsif os == "windows"
+				nodepath = File.join(Paths.resource_dir, "nodejs", "node.exe")
+				`#{nodepath} #{js} #{html}`
+			else
+				File.open(Bkmkr::Paths.log_file, 'a+') do |f|
+					f.puts "----- NODE ERROR"
+					f.puts "ERROR: I can't seem to run node. Is it installed and part of your system PATH?"
+					f.puts "ABORTING. All following processes will fail."
+				end
+				File.delete(Project.alert)
+			end
 		end
 		def self.insertaddons(inputfile, sectionparams, addonparams)
 			# The section types JSON
