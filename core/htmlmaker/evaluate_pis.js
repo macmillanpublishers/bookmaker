7eda1bf9ed30848c52207fef29443f11ba680d3a
nelliemckesson <nellie.mckesson@macmillan.com>
2016-01-20 10:02:28 -0500

    adding trim pi handling

--- core/htmlmaker/evaluate_pis.js
+++ core/htmlmaker/evaluate_pis.js
@@ -12,8 +12,15 @@ fs.readFile(file, function processTemplates (err, contents) {
       var val = $( this ).text()
       if (val = "Ebook-only") {
         $( this ).parent().attr('data-format','ebook')
-      } else if ($( this ).text() = "Print-only") {
+      } 
+      if (val = "Print-only") {
         $( this ).parent().attr('data-format','print')
+      }
+      if (val.indexOf("TRIM:") > -1) {
+        var trimsize = val.split(":").pop().replace(/\s+/g, '');
+        trimsize = trimsize.replace(/x/g, ' ');
+        var metabooktrim = '<meta name="size" content="' + trimsize + '"/>';
+        $('head').append(metabooktrim);
       }
       $(this).remove();
   });
