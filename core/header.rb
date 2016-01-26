a6a52f23771ca3840fdb5532bc0b12e8ec56e3c0
nelliemckesson <nellie.mckesson@macmillan.com>
2016-01-26 13:19:57 -0500

    troubleshooting

--- core/header.rb
+++ core/header.rb
@@ -346,9 +346,10 @@ module Bkmkr
 						# copy any images to epub conversion dir
 						epub_img_dir = File.join(Bkmkr::Paths.project_tmp_dir, "epubimg")
 						images = []
-						images = addoncontent.scan(/<img/)
+						images = File.read(addonfile).scan(/<img/)
 						if images.any?
 							images.each do |i|
+								puts i
 								source = i.match(/(src=")(.*?)(")/)[2]
 								imagepath = File.join(addonfiledir, "images", source)
 								fileutils.cp(imagepath, epub_img_dir)
