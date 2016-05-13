b8fcd1fbf2decccac229d2d15ff749f5eb688dfb
nelliemckesson <nellie.mckesson@macmillan.com>
2016-05-13 11:56:05 -0400

    revising section sequence logic

--- core/utilities/movesection.js
+++ core/utilities/movesection.js
@@ -4,11 +4,21 @@ var file = process.argv[2];
 var srcEl = process.argv[3];
 var srcType = process.argv[4];
 var srcClass = process.argv[5];
-var ss = process.argv[6] - 1;
+if (process.argv[6] > 0) {
+  var num = process.argv[6] - 1;
+  var ss = "[" + num.toString() + "]";
+} else {
+  var ss = "";
+}
 var destEl = process.argv[7];
 var destType = process.argv[8];
 var destClass = process.argv[9];
-var ds = process.argv[10] - 1;
+if (process.argv[10] > 0) {
+  var num2 = process.argv[10] - 1;
+  var ds = "[" + num2.toString() + "]";
+} else {
+  var ds = "";
+}
 
 fs.readFile(file, function moveSection (err, contents) {
   $ = cheerio.load(contents, {
@@ -16,23 +26,23 @@ fs.readFile(file, function moveSection (err, contents) {
         });
 
   if (srcType && srcClass) { 
-  	var source = $(srcEl + '[class="' + srcClass + '"][data-type="' + srcType + '"]')[ss]; 
+  	var source = $(srcEl + '[class="' + srcClass + '"][data-type="' + srcType + '"]') + ss; 
   } else if (srcType && !srcClass) {
-  	var source = $(srcEl + '[data-type="' + srcType + '"]')[ss];
+  	var source = $(srcEl + '[data-type="' + srcType + '"]') + ss;
   } else if (!srcType && srcClass) {
-  	var source = $(srcEl + '[class="' + srcClass + '"]')[ss];
+  	var source = $(srcEl + '[class="' + srcClass + '"]') + ss;
   } else {
-  	var source = $(srcEl)[ss];
+  	var source = $(srcEl) + ss;
   };
 
   if (destType && destClass) { 
-    var destination = $(destEl + '[class="' + destClass + '"][data-type="' + destType + '"]')[ds]; 
+    var destination = $(destEl + '[class="' + destClass + '"][data-type="' + destType + '"]') + ds; 
   } else if (destType && !destClass) {
-    var destination = $(destEl + '[data-type="' + destType + '"]')[ds];
+    var destination = $(destEl + '[data-type="' + destType + '"]') + ds;
   } else if (!destType && destClass) {
-    var destination = $(destEl + '[class="' + destClass + '"]')[ds];
+    var destination = $(destEl + '[class="' + destClass + '"]') + ds;
   } else {
-    var destination = $(destEl)[ds];
+    var destination = $(destEl) + ds;
   };
   
   if (destEl == "body") {
