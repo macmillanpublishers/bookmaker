7bb5b62d5abdf86b73f28374388d82178496288b
MacmillanWorkflows <workflows@macmillan.com>
2016-08-25 14:32:22 -0400

    more elegant solution than the last commit

--- core/header.rb
+++ core/header.rb
@@ -21,7 +21,7 @@ module Bkmkr
 		def self.filename_split
 			@@filename_split
 		end
-		@@filename = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).pop.split(".").reverse.drop(1).reverse.join('.').gsub(/ /, "")
+		@@filename = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).pop.rpartition('.').first.gsub(/ /, "")
 		def self.filename
 			@@filename
 		end
