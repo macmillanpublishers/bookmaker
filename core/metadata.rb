b65d199290f8baea526d27eab43a1ed6e74e85da
matt.retzer <matthew.retzer@macmillan.com>
2016-12-02 14:24:48 -0500

    adding some definitions to metadata.rb so we don't have to read the whole config file into some addons scripts

--- core/metadata.rb
+++ core/metadata.rb
@@ -14,7 +14,7 @@ class Metadata
 	def self.booktitle
 		if @@data_hash['title'].nil? or @@data_hash['title'].empty? or !@@data_hash['title']
 			Bkmkr::Project.filename
-		else 
+		else
 			@@data_hash['title']
 		end
 	end
@@ -22,7 +22,7 @@ class Metadata
 	def self.booksubtitle
 		if @@data_hash['subtitle'].nil? or @@data_hash['subtitle'].empty? or !@@data_hash['subtitle']
 			"Unknown"
-		else 
+		else
 			@@data_hash['subtitle']
 		end
 	end
@@ -30,7 +30,7 @@ class Metadata
 	def self.bookauthor
 		if @@data_hash['author'].nil? or @@data_hash['author'].empty? or !@@data_hash['author']
 			"Unknown"
-		else 
+		else
 			@@data_hash['author']
 		end
 	end
@@ -38,7 +38,7 @@ class Metadata
 	def self.productid
 		if @@data_hash['productid'].nil? or @@data_hash['productid'].empty? or !@@data_hash['productid']
 			Bkmkr::Project.filename
-		else 
+		else
 			@@data_hash['productid']
 		end
 	end
@@ -46,7 +46,7 @@ class Metadata
 	def self.pisbn
 		if @@data_hash['printid'].nil? or @@data_hash['printid'].empty? or !@@data_hash['printid']
 			Bkmkr::Project.filename
-		else 
+		else
 			@@data_hash['printid']
 		end
 	end
@@ -54,7 +54,7 @@ class Metadata
 	def self.eisbn
 		if @@data_hash['ebookid'].nil? or @@data_hash['ebookid'].empty? or !@@data_hash['ebookid']
 			Bkmkr::Project.filename
-		else 
+		else
 			@@data_hash['ebookid']
 		end
 	end
@@ -62,7 +62,7 @@ class Metadata
 	def self.imprint
 		if @@data_hash['imprint'].nil? or @@data_hash['imprint'].empty? or !@@data_hash['imprint']
 			"Unknown"
-		else 
+		else
 			@@data_hash['imprint']
 		end
 	end
@@ -70,7 +70,7 @@ class Metadata
 	def self.publisher
 		if @@data_hash['publisher'].nil? or @@data_hash['publisher'].empty? or !@@data_hash['publisher']
 			"Unknown"
-		else 
+		else
 			@@data_hash['publisher']
 		end
 	end
@@ -78,7 +78,7 @@ class Metadata
 	def self.printcss
 		if @@data_hash['printcss'].nil? or @@data_hash['printcss'].empty? or !@@data_hash['printcss']
 			"none"
-		else 
+		else
 			@@data_hash['printcss']
 		end
 	end
@@ -86,7 +86,7 @@ class Metadata
 	def self.printjs
 		if @@data_hash['printjs'].nil? or @@data_hash['printjs'].empty? or !@@data_hash['printjs']
 			"none"
-		else 
+		else
 			@@data_hash['printjs']
 		end
 	end
@@ -94,7 +94,7 @@ class Metadata
 	def self.epubcss
 		if @@data_hash['ebookcss'].nil? or @@data_hash['ebookcss'].empty? or !@@data_hash['ebookcss']
 			"none"
-		else 
+		else
 			@@data_hash['ebookcss']
 		end
 	end
@@ -102,7 +102,7 @@ class Metadata
 	def self.frontcover
 		if @@data_hash['frontcover'].nil? or @@data_hash['frontcover'].empty? or !@@data_hash['frontcover']
 			"Unknown"
-		else 
+		else
 			@@data_hash['frontcover']
 		end
 	end
@@ -110,7 +110,7 @@ class Metadata
 	def self.epubtitlepage
 		if @@data_hash['epubtitlepage'].nil? or @@data_hash['epubtitlepage'].empty? or !@@data_hash['epubtitlepage']
 			"Unknown"
-		else 
+		else
 			@@data_hash['epubtitlepage']
 		end
 	end
@@ -118,8 +118,32 @@ class Metadata
 	def self.podtitlepage
 		if @@data_hash['podtitlepage'].nil? or @@data_hash['podtitlepage'].empty? or !@@data_hash['podtitlepage']
 			"Unknown"
-		else 
+		else
 			@@data_hash['podtitlepage']
+		end
+	end
+
+	def self.project_dir
+		if @@data_hash['project'].nil? or @@data_hash['project'].empty? or !@@data_hash['project']
+			"bookmaker"
+		else
+			@@data_hash['project']
+		end
+	end
+
+	def self.stage_dir
+		if @@data_hash['stage'].nil? or @@data_hash['stage'].empty? or !@@data_hash['stage']
+			"final"
+		else
+			@@data_hash['stage']
+		end
+	end
+
+	def self.pod_toc
+		if @@data_hash['pod_toc'].nil? or @@data_hash['pod_toc'].empty? or !@@data_hash['pod_toc']
+			"false"
+		else
+			@@data_hash['stage']
 		end
 	end
 end
