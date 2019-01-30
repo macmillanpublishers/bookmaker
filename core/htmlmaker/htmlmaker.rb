1f1ef13be01adc424bc7cc872cf70fa6301f8cdc
Matt Retzer <matthew.retzer@macmillan.com>
2019-01-30 15:11:02 -0500

    making preformatted.js non-rsuite-only

--- core/htmlmaker/htmlmaker.rb
+++ core/htmlmaker/htmlmaker.rb
@@ -240,8 +240,11 @@ overwriteFile(Bkmkr::Paths.outputtmp_html, filecontents, 'overwrite_output_html_
 # # add correct markup for inlines (em, strong, sup, sub)
 htmlmakerRunNode(inlines_js, "#{Bkmkr::Paths.outputtmp_html} #{doctemplatetype}", 'inlines_js')
 
-# # # change p children of pre tags to spans
-# htmlmakerRunNode(preformatted_js, Bkmkr::Paths.outputtmp_html, 'preformatted_js')
+# since in rsuite we are using <pre> blocks as containers, this makes a mess of enclosed <p>s
+unless doctemplatetype == 'rsuite'
+  # # change p children of pre tags to spans
+  htmlmakerRunNode(preformatted_js, Bkmkr::Paths.outputtmp_html, 'preformatted_js')
+end
 
 # for xsl-only: I think this includes stuff from formerly included:
   # footnotes.js, lists.js, parts.js, strip-toc.js, headings.js(for xsl) and some from band-aid.js
