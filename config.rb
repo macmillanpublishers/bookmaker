f74c7a4a4d3f8ac7e4730c767cf7b8dc3bd50031
Matt Retzer <matthew.retzer@macmillan.com>
2019-09-23 14:53:16 -0400

    hardcoding S partition for tmpdir, removing currvolcheck b/c it _shouldn't_ matter (ran some tests)

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # ------------------ CUSTOM VARIABLES
 # Add any custom variables you'd like to use in the global variables below.
-$currpath = Dir.pwd
-$currvol = $currpath.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).shift
+# $currpath = Dir.pwd
+# $currvol = $currpath.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).shift
 
 unless ARGV.empty?		#adding this check for testing purposes
   unescapeargv = ARGV[0].chomp('"').reverse.chomp('"').reverse
@@ -27,7 +27,7 @@ $op_system = "windows"
 # The location of the temporary working folder.
 # This is where bookmaker will perform most actions
 # before moving the finalized files to the "done" directory.
-$tmp_dir = File.join($currvol, "bookmaker_tmp", project)
+$tmp_dir = File.join("S:", "bookmaker_tmp", project)
 
 # The location to store the log file that gets created
 # for each conversion.
--- core/tmparchive/tmparchive_rsuite.rb
+++ core/tmparchive/tmparchive_rsuite.rb
@@ -2,93 +2,93 @@ require 'fileutils'
 
 require_relative '../header.rb'
 
-puts "inputfile: ", "#{Bkmkr::Project.input_file_normalized}"
-puts "project tmpdir:", ARGV[1].chomp('"').reverse.chomp('"').reverse
-sleep(20)
-puts "I'm Done!"
+# puts "inputfile: ", "#{Bkmkr::Project.input_file_normalized}"
+# puts "project tmpdir:", ARGV[1].chomp('"').reverse.chomp('"').reverse
+# sleep(20)
+# puts "I'm Done!"
 # ---------------------- VARIABLES
-# local_log_hash, @log_hash = Bkmkr::Paths.setLocalLoghash(true)
-#
-# input_config = File.join(Bkmkr::Paths.submitted_images, "config.json")
-#
-# tmp_config = File.join(Bkmkr::Paths.project_tmp_dir, "config.json")
-#
-# # ---------------------- METHODS
-# ## all methods for this script are Mcmlln::Tools methods wrapped in new methods,
-# ## in order to return results for json_logfile
-# def getFilesinSubmittedImages(logkey='')
-# 	files = Mcmlln::Tools.dirList(Bkmkr::Paths.submitted_images)
-# 	files
-# rescue => logstring
-# 	return []
-# ensure
-# 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
-# end
-#
-# def makeFolder(path, logkey='')
-# 	unless Dir.exist?(path)
-# 		Mcmlln::Tools.makeDir(path)
-# 	else
-# 	 logstring = 'n-a'
-# 	end
-# rescue => logstring
-# ensure
-# 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
-# end
-#
-# def deleteOldProjectTmpFolders(project_tmp_dir, logkey='')
-#   pathroot, u, count = project_tmp_dir.rpartition('_')
-#   dircount = 0
-#   Dir.glob("#{pathroot}*") do |p_tmpdir|
-#       if p_tmpdir.rpartition('_')[2].to_i > count.to_i
-#         dircount += 1
-#         Mcmlln::Tools.deleteDir(p_tmpdir)
-#       end
-#   end
-#   if dircount > 0
-#     logstring = "deleted #{dircount} tmpdir(s) with higher increment than new one"
-#   end
-# rescue => logstring
-# ensure
-# 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
-# end
-#
-# def copyInputFile(logkey='')
-# 	Mcmlln::Tools.copyFile("#{Bkmkr::Project.input_file}", Bkmkr::Paths.project_tmp_file)
-# rescue => logstring
-# ensure
-# 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
-# end
-#
-# def mvInputConfigFile(input_config, tmp_config, logkey='')
-# 	if File.file?(input_config)
-# 		Mcmlln::Tools.moveFile(input_config, tmp_config)
-# 	else
-# 		logstring = 'n-a'
-# 	end
-# rescue => logstring
-# ensure
-# 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
-# end
-#
-# def mvSubmittedFiles(dir, dest, logkey='')
-#   Mcmlln::Tools.copyAllFiles(dir, dest)
-#   Dir.foreach(dir) {|f|
-#     fn = File.join(dir, f)
-#     File.delete(fn) if !File.directory?(fn)
-#   }
-# rescue => logstring
-# ensure
-# 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
-# end
-#
-# def writeAlertFile(filecontents, logkey='')
-# 	Mcmlln::Tools.overwriteFile(Bkmkr::Paths.alert, filecontents)
-# rescue => logstring
-# ensure
-# 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
-# end
-#
+local_log_hash, @log_hash = Bkmkr::Paths.setLocalLoghash(true)
+
+input_config = File.join(Bkmkr::Paths.submitted_images, "config.json")
+
+tmp_config = File.join(Bkmkr::Paths.project_tmp_dir, "config.json")
+
+# ---------------------- METHODS
+## all methods for this script are Mcmlln::Tools methods wrapped in new methods,
+## in order to return results for json_logfile
+def getFilesinSubmittedImages(logkey='')
+	files = Mcmlln::Tools.dirList(Bkmkr::Paths.submitted_images)
+	files
+rescue => logstring
+	return []
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
+def makeFolder(path, logkey='')
+	unless Dir.exist?(path)
+		Mcmlln::Tools.makeDir(path)
+	else
+	 logstring = 'n-a'
+	end
+rescue => logstring
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
+def deleteOldProjectTmpFolders(project_tmp_dir, logkey='')
+  pathroot, u, count = project_tmp_dir.rpartition('_')
+  dircount = 0
+  Dir.glob("#{pathroot}*") do |p_tmpdir|
+      if p_tmpdir.rpartition('_')[2].to_i > count.to_i
+        dircount += 1
+        Mcmlln::Tools.deleteDir(p_tmpdir)
+      end
+  end
+  if dircount > 0
+    logstring = "deleted #{dircount} tmpdir(s) with higher increment than new one"
+  end
+rescue => logstring
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
+def copyInputFile(logkey='')
+	Mcmlln::Tools.copyFile("#{Bkmkr::Project.input_file}", Bkmkr::Paths.project_tmp_file)
+rescue => logstring
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
+def mvInputConfigFile(input_config, tmp_config, logkey='')
+	if File.file?(input_config)
+		Mcmlln::Tools.moveFile(input_config, tmp_config)
+	else
+		logstring = 'n-a'
+	end
+rescue => logstring
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
+def mvSubmittedFiles(dir, dest, logkey='')
+  Mcmlln::Tools.copyAllFiles(dir, dest)
+  Dir.foreach(dir) {|f|
+    fn = File.join(dir, f)
+    File.delete(fn) if !File.directory?(fn)
+  }
+rescue => logstring
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
+def writeAlertFile(filecontents, logkey='')
+	Mcmlln::Tools.overwriteFile(Bkmkr::Paths.alert, filecontents)
+rescue => logstring
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
 # # ---------------------- PROCESSES
 # # Local path variables
 # all_submitted_images = getFilesinSubmittedImages('check_submitted_images')
