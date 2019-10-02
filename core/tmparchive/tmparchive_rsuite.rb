b9cd81ff446e85ec73bfe085e4c31515daa7fab8
Matt Retzer <matthew.retzer@macmillan.com>
2019-10-02 15:31:48 -0400

    adding write of rs_servername to bkmkrmetadata.json

--- core/tmparchive/tmparchive_rsuite.rb
+++ core/tmparchive/tmparchive_rsuite.rb
@@ -2,28 +2,33 @@ require 'fileutils'
 
 require_relative '../header.rb'
 
-# puts "inputfile: ", "#{Bkmkr::Project.input_file_normalized}"
-# puts "project tmpdir:", ARGV[1].chomp('"').reverse.chomp('"').reverse
-# sleep(20)
-# puts "I'm Done!"
+
 # ---------------------- VARIABLES
 
 local_log_hash, @log_hash = Bkmkr::Paths.setLocalLoghash(true)
 
 tmpdir_from_rsuite = ARGV[1].chomp('"').reverse.chomp('"').reverse.gsub('\\', '/')
 
+rs_server = ARGV[2].chomp('"').reverse.chomp('"').reverse
+
 input_config = File.join(Bkmkr::Paths.project_tmp_dir_submitted, "config.json")
 
 tmp_config = File.join(Bkmkr::Paths.project_tmp_dir, "config.json")
 
 
-# Mcmlln::Tools.logtoJson(@log_hash, 'infile', Bkmkr::Project.input_file_normalized)
-# Mcmlln::Tools.logtoJson(@log_hash, 'project_tmpdir', Bkmkr::Paths.project_tmp_dir)
-# Mcmlln::Tools.logtoJson(@log_hash, 'project_tmpdir2', ARGV[1].chomp('"').reverse.chomp('"').reverse)
-
 # ---------------------- METHODS
-## all methods for this script are Mcmlln::Tools methods wrapped in new methods,
-## in order to return results for json_logfile
+## most methods for this script are Mcmlln::Tools methods wrapped in new methods,
+##  in order to return results for json_logfile
+
+def readJson(jsonfile, logkey='')
+  data_hash = Mcmlln::Tools.readjson(jsonfile)
+  return data_hash
+rescue => logstring
+  return {}
+ensure
+  Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
 def getSubmittedFilesList(dirname, logkey='')
 	files = Mcmlln::Tools.dirList(dirname)
 	files
@@ -61,13 +66,6 @@ ensure
 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
 end
 
-# def copyInputFile(logkey='')
-# 	Mcmlln::Tools.copyFile("#{Bkmkr::Project.input_file}", Bkmkr::Paths.project_tmp_file)
-# rescue => logstring
-# ensure
-# 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
-# end
-
 def mvInputConfigFile(input_config, tmp_config, logkey='')
 	if File.file?(input_config)
 		Mcmlln::Tools.moveFile(input_config, tmp_config)
@@ -79,17 +77,6 @@ ensure
 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
 end
 
-# def mvSubmittedFiles(dir, dest, logkey='')
-#   Mcmlln::Tools.copyAllFiles(dir, dest)
-#   Dir.foreach(dir) {|f|
-#     fn = File.join(dir, f)
-#     File.delete(fn) if !File.directory?(fn)
-#   }
-# rescue => logstring
-# ensure
-# 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
-# end
-
 # def writeAlertFile(filecontents, logkey='')
 # 	Mcmlln::Tools.overwriteFile(Bkmkr::Paths.alert, filecontents)
 # rescue => logstring
@@ -97,7 +84,21 @@ end
 # 	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
 # end
 
+def writeHashToJSON(hash, jsonfile, logkey='')
+  if not hash.empty?
+    Mcmlln::Tools.write_json(hash, jsonfile)
+  else
+    logstring = 'no data to write to json (empty hash)'
+  end
+rescue => logstring
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
 # # ---------------------- PROCESSES
+# local definitions from json files
+rsuite_metadata_hash = readJson(Bkmkr::Paths.fromrsuite_Metadata_json, 'read_rsuite_metadata_json')
+
 # log some basic info to json:
 @log_hash['infile'] = Bkmkr::Project.input_file_normalized
 @log_hash['tmpdir_from_rsuite'] = tmpdir_from_rsuite
@@ -107,34 +108,26 @@ if tmpdir_from_rsuite != Bkmkr::Paths.project_tmp_dir
   @log_hash['tmpdirs_dont_match'] = "tmpdir_from_rsuite does not match bkmkr tmp_dir: #{Bkmkr::Paths.project_tmp_dir}"
 else
   all_submitted_files = getSubmittedFilesList(Bkmkr::Paths.project_tmp_dir_submitted, 'check_submitted_files_besides_docx')
-  # log submitted image list
+  # log submitted files list
   @log_hash['submitted_files'] = all_submitted_files
 
-# makeFolder(Bkmkr::Paths.tmp_dir, 'tmp_folder_created')
-#
-# # make project tmp folder: create new if current exists.
-# makeFolder(Bkmkr::Paths.project_tmp_dir, 'project_tmp_folder_created')
-
-# rm any old unique tmp folders for this project with higher increments
+  # rm any old unique tmp folders for this project with higher increments
   deleteOldProjectTmpFolders(Bkmkr::Paths.project_tmp_dir, 'old_project_tmp_folders_delete')
 
+  # create necessary subdir
   makeFolder(Bkmkr::Paths.project_tmp_dir_img, 'project_tmp_img_folder_created')
 
-# makeFolder(Bkmkr::Paths.project_tmp_dir_submitted, 'project_tmp_submitted_folder created')
-
+  # move input config file to root of tmpdir (if present)
   mvInputConfigFile(input_config, tmp_config, 'moved_input_config_file')
 
-# # Rename and move input files to tmp folder to eliminate possibility of overwriting
-# copyInputFile('copy_input_file')
-#
-# # move all submitted files to project_tmp_dir_submitted
-# # => except input file and config.json (already moved above)
-# mvSubmittedFiles(Bkmkr::Paths.submitted_images, Bkmkr::Paths.project_tmp_dir_submitted, 'moving_submitted_items_to_tmp')
-#
-# filecontents = "The conversion processor is currently running. Please do not submit any new files or images until the process completes."
-#
-# writeAlertFile(filecontents, 'write_alert_file')
+  # # write bookmaker 'busy' file to project dir <-- not really supported for simultaneous runs, but leaving, commented, in case we want to rework
+  # filecontents = "The conversion processor is currently running. Please do not submit any new files or images until the process completes."
+  # writeAlertFile(filecontents, 'write_alert_file')
 
+  # write rs_servername value to metadata_json
+  @log_hash['rsuite_server'] = rs_server
+  rsuite_metadata_hash['rsuite_server'] = rs_server
+  writeHashToJSON(rsuite_metadata_hash, Bkmkr::Paths.fromrsuite_Metadata_json, 'write_RSserver_info_to_json')
 end
 
 # ---------------------- LOGGING
